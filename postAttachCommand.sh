cfd2216bad22fc8bb5fb9e996e8f67886ea479d0
Sebastian Zeuch <sebastian.zeuch@icloud.com>
2025-10-22 10:52:05 +0200

    fix(devcontainer): remove udev rules triggering from postAttachCommand.sh

--- postAttachCommand.sh
+++ postAttachCommand.sh
@@ -1,8 +1,1 @@
 #!/bin/bash
-
-# Trigger udev rules on container start
-echo "#=============================================================================="
-echo "Trigger udev rules"
-echo "#=============================================================================="
-sudo service udev restart
-sudo udevadm control --reload-rules && sudo udevadm trigger
